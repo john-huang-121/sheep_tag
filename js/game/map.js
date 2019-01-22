582dc0190db9b767fb71772871a0c3ac35748773
john <johnhuang121@gmail.com>
2019-01-22 15:17:44 -0800

    add 10x10px grid system to map class

--- js/game/map.js
+++ js/game/map.js
@@ -3,10 +3,22 @@
 export default class Map {
   constructor(ctx) {
     this.ctx = ctx;
+    this.grid = this.generateGrid();
     this.x = 900;
     this.y = 900;
 
+    this.generateGrid = this.generateGrid.bind(this);
     this.drawMap = this.drawMap.bind(this);
+  }
+
+  generateGrid() {
+    let array = new Array(9); // 9x9 deep with 10px, 10px as each grid block
+
+    for (let i = 0; i < 9; i++) {
+      array[i] = new Array();
+    }
+
+    return array;
   }
 
   drawMap() {
